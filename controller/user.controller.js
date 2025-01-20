057cb8ac5b9f121f7ddff129f04e9dfce8a10e6a
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2025-01-20 22:27:55 +0500

    env and user contoller (login function)

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,7 @@
 const user = require('../models/user.model')
-
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
 const register = async (req,res)=>{
     try{
         const data = req.body
@@ -18,10 +20,17 @@ const register = async (req,res)=>{
 
 const login = async (req,res)=>{
     try{
+        const data = req.body
+        const email = await user.findOne({email:data.email})
 
+        if(email && validate(email.password,data.password)){
+            const token = jwt.sign({id:email._id},process.env.Jwt_key)
+            res.status(200).json({"message":"login successfull",token:token})
+        }
     }
     catch(error){
-        
+            console.log(error)
+            res.status(500).json("Error in login")
     }
 }
 
