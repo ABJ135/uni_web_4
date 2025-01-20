1d5fc0de4ae5ea71215615faaec5df4ef8e72bb1
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2025-01-21 01:54:36 +0500

    project router

--- router/project.router.js
+++ router/project.router.js
@@ -5,4 +5,12 @@ const controller = require("../controller/project.controller");
 
 router.post('/createProject',auth,controller.createProject)
 
+router.get('/getProject',auth,controller.getProject)
+
+router.get('/getProjects/:id',auth,controller.getProjects)
+
+router.put('/updateProject',auth,controller.updateProject)
+
+router.delete('/deleteProject',auth,controller.deleteProject)
+
 module.exports = router;
