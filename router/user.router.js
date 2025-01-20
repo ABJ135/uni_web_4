82f02d63adf57cb53d91dc5c32d24e0f1caeeda7
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2025-01-20 22:12:22 +0500

    user router started

--- router/user.router.js
+++ router/user.router.js
@@ -5,3 +5,8 @@ const controller = require('../controller/user.controller')
 
 router.post('/register',controller.register)
 
+router.get('/login',controller.login)
+
+router.get('/profile',controller.profile)
+
+module.exports = router
